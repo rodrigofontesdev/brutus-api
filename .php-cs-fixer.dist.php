377f1cedfc4537c874d356707a6cd5e3efd29c83
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-11-07 18:51:57 -0300

    style: exclude storage and public directories from PHP CS Fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,8 +1,16 @@
 <?php
 
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude([
+        'storage',
+        'public'
+    ]);
+
 return (new PhpCsFixer\Config())
     ->setParallelConfig(new PhpCsFixer\Runner\Parallel\ParallelConfig(4, 20))
     ->setUsingCache(true)
     ->setRules([
         '@Symfony' => true
-    ]);
+    ])
+    ->setFinder($finder);
