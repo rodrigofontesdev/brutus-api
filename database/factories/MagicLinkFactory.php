e65703223e16e654c24a27ea81332d751999251f
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-11-21 18:41:40 -0300

    refactor(factory): use expire time generator in magic link factory

--- database/factories/MagicLinkFactory.php
+++ database/factories/MagicLinkFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Helpers\Generator;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -13,20 +14,20 @@ class MagicLinkFactory extends Factory
     {
         return [
             'used_at' => null,
-            'expires_at' => now()->addMinutes(5)->toDateTimeString(),
+            'expires_at' => Generator::magicLinkExpireTime(),
         ];
     }
 
     public function used(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn () => [
             'used_at' => now()->toDateTimeString(),
         ]);
     }
 
     public function expired(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn () => [
             'expires_at' => now()->subDay()->toDateTimeString(),
         ]);
     }
