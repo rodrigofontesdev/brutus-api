7a554e15155aec5dadbfc75ddca0ed70d82dcea2
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-11-30 14:46:32 -0300

    style: disable rule in PHP CS Fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,6 +11,7 @@ return (new PhpCsFixer\Config())
     ->setParallelConfig(new PhpCsFixer\Runner\Parallel\ParallelConfig(4, 20))
     ->setUsingCache(true)
     ->setRules([
-        '@Symfony' => true
+        '@Symfony' => true,
+        'no_multiline_whitespace_around_double_arrow' => false,
     ])
     ->setFinder($finder);
